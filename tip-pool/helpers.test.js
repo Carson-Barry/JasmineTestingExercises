77fa7044815c070e7f8c13129768bc24c8ecb1eb
Carson Barry <carsonbarry@Carsons-Air.lan>
2023-05-28 11:25:34 -0500

    created placeholder files for helpers.test and payments.test, continuing work

---
+++ tip-pool/helpers.test.js
@@ -0,0 +1,24 @@
+describe("Helpers test (with setup and tear-down)", function() {
+    beforeEach(function () {
+      // initialization logic
+
+    });
+  
+    it('placeholder 1', function () {
+        
+    });
+  
+    it("placeholder 2", function() {
+
+    });
+
+    it("placeholder 3", function() {
+
+    });
+  
+    afterEach(function() {
+      // teardown logic
+
+    });
+  });
+  
---
+++ tip-pool/payments.test.js
@@ -0,0 +1,28 @@
+describe("Payments test (with setup and tear-down)", function() {
+    beforeEach(function () {
+      // initialization logic
+
+    });
+  
+    it('placeholder 1', function () {
+        
+    });
+  
+    it("placeholder 2", function() {
+
+    });
+
+    it("placeholder 3", function() {
+
+    });
+
+    it("placeholder 3", function() {
+
+    });
+  
+    afterEach(function() {
+      // teardown logic
+
+    });
+  });
+  
