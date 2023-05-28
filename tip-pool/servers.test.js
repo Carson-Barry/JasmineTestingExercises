a60ee4e5a51d51d8ea415f2bc46b23456ba58955
Carson Barry <carsonbarry@Carsons-Air.lan>
2023-05-28 11:13:56 -0500

    finished servers.test.js

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -11,7 +11,21 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it("should update serverTable when an update is submitted on updateServerTable", function() {
+    submitServerInfo();
+    updateServerTable();
+
+    let serverTableTds = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(serverTableTds[0].innerHTML).toEqual('Alice');
+    expect(serverTableTds[1].innerHTML).toEqual('$0.00');
+
+  });
+
   afterEach(function() {
     // teardown logic
+    allServers = {};
+    serverTbody.innerHTML="";
+    serverId = 0;
   });
 });
